b201478e7552fc385aae46e5e82977d077e55001
Keanu Hie <hie.keanu@gmail.com>
2025-02-20 13:25:48 +0100

    fix: colors in deploy not shown

--- components/main-section/deploy/DeployIcon.vue
+++ components/main-section/deploy/DeployIcon.vue
@@ -2,8 +2,8 @@
   <div
     class="relative z-10 flex h-12 w-12 items-center justify-center rounded-lg transition-all duration-300"
     :class="{
-      [`scale-110 !bg-${color}-500/50 !text-${color}-300`]: currentStep >= activateStep,
-      [`bg-${color}-500/20 text-${color}-400`]: true,
+      [activeColorClassNames[color]]: currentStep >= activateStep,
+      [colorClassNames[color]]: true,
     }"
   >
     <slot />
@@ -18,4 +18,22 @@ export interface DeployIconProps {
 }
 
 defineProps<DeployIconProps>()
+
+const colorClassNames = computed(() => {
+  return {
+    blue: 'bg-blue-500/20 text-blue-400',
+    orange: 'bg-orange-500/20 text-orange-400',
+    red: 'bg-red-500/20 text-red-400',
+    green: 'bg-green-500/20 text-green-400',
+  }
+})
+
+const activeColorClassNames = computed(() => {
+  return {
+    blue: '!bg-blue-500/50 !text-blue-300',
+    orange: '!bg-orange-500/50 !text-orange-300',
+    red: '!bg-red-500/50 !text-red-300',
+    green: '!bg-green-500/50 !text-green-300',
+  }
+})
 </script>
