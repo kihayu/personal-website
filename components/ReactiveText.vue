f4c57ecd45b88b4e008e7242ce45cc0b31a2e094
Keanu Hie <hie.keanu@gmail.com>
2025-02-22 00:12:19 +0100

    refactor: simplify reactive text

--- components/ReactiveText.vue
+++ components/ReactiveText.vue
@@ -1,12 +1,9 @@
 <template>
   <div
-    class="group flex w-full cursor-pointer items-center justify-between rounded-lg border border-neutral-400 bg-stone-800 p-4 transition-all duration-300 hover:border-neutral-200 active:scale-[0.98] active:bg-stone-700 lg:w-fit lg:border-none lg:bg-transparent lg:p-0 lg:active:scale-100 lg:active:bg-transparent"
+    class="flex w-full cursor-pointer items-center justify-between rounded-lg border border-neutral-400 bg-stone-800 p-4 transition-all duration-300 hover:border-neutral-200 active:scale-[0.98] active:bg-stone-700 lg:w-fit lg:border-none lg:bg-transparent lg:p-0 lg:active:scale-100 lg:active:bg-transparent"
   >
     <h3
-      class="font-title text-2xl font-bold md:text-3xl xl:text-4xl"
-      :class="{
-        'lg:transition-transform lg:duration-300 lg:group-hover:translate-x-3': !isMobile,
-      }"
+      class="font-title text-2xl font-bold select-none md:text-3xl lg:transition-all lg:duration-150 lg:hover:text-neutral-400 lg:active:text-neutral-500 xl:text-4xl"
       @mouseover="onMouseEnter"
       @mouseleave="onMouseLeave"
       @focusin="onMouseEnter"
@@ -15,17 +12,10 @@
     >
       <slot />
     </h3>
-    <span
-      class="text-2xl text-neutral-400 transition-all duration-300 group-hover:text-neutral-200 lg:absolute lg:-translate-x-9 lg:text-3xl lg:opacity-0 lg:group-hover:-translate-x-6 lg:group-hover:opacity-100"
-    >
-      →
-    </span>
   </div>
 </template>
 
 <script setup lang="ts">
-import { isMobile } from '~/utils/isMobile'
-
 export interface ReactiveTextProps {
   name: string
 }
