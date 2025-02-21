086e4e3e8a9680b851701816a50e74ea3de0ac38
Keanu Hie <hie.keanu@gmail.com>
2025-02-22 00:17:03 +0100

    fix: too many paragraphs in about me text

--- components/AboutMeCard.vue
+++ components/AboutMeCard.vue
@@ -4,20 +4,19 @@
     <div class="mt-3 space-y-4 will-change-contents" aria-labelledby="about-section">
       <p class="font-paragraph text-lg will-change-contents md:text-xl">
         Hi, I'm a Fullstack Developer with a focus on Frontend and Web-Development, specifically UI/UX, functionality
-        and clean code.
-      </p>
-      <p class="font-paragraph text-lg will-change-contents md:text-xl">
-        With over 7 years of professional experience, I have a good understanding of diverse frontend topics such as
-        Styleguides, Accessibility, Responsive Design, and Component-Driven Development.
-      </p>
-      <p class="font-paragraph text-lg will-change-contents md:text-xl">
-        Most of this experience was done in Vue.js with SCSS or Tailwind, as well as Storybook on larger projects,
-        including React and Next.js on a smaller scale.
-      </p>
-      <p class="font-paragraph text-lg will-change-contents md:text-xl">
-        Also working on backend technologies is nothing new for me as I've already built diverse API's and
-        Microservices, as well as helped with the architecture of complex systems.
+        and clean code. With over 12 years in the business and 7 years of professional experience, I have a good
+        understanding of diverse frontend topics such as Styleguides, Accessibility, Responsive Design, and
+        Component-Driven Development. Most of this experience was done in Vue.js with SCSS or Tailwind, as well as
+        Storybook on larger projects, including React and Next.js on a smaller scale. Also working on backend
+        technologies is nothing new for me as I've already built diverse API's and Microservices, as well as helped with
+        the architecture of complex systems.
       </p>
     </div>
   </div>
 </template>
+
+<style>
+.font-paragraph {
+  font-display: swap;
+}
+</style>
