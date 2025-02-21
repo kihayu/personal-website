ffd7f580553c3df4ec14b9b95db3dfcb5973e421
Keanu Hie <hie.keanu@gmail.com>
2025-02-21 12:07:03 +0100

    refactor: adjust global max-width

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <MainSection class="mx-4 max-w-[1440px] py-4 lg:mx-auto lg:pt-8" />
+  <MainSection class="mx-4 max-w-[1280px] py-4 lg:mx-auto lg:pt-8" />
 </template>
 
 <script setup lang="ts">
