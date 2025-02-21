873dee461fa48fbc894583b1b16257ba10e6870d
Keanu Hie <hie.keanu@gmail.com>
2025-02-21 12:06:11 +0100

    chore: wording change in AboutMe card

--- components/AboutMeCard.vue
+++ components/AboutMeCard.vue
@@ -3,8 +3,8 @@
     <h2 class="font-title text-3xl font-semibold" id="about-section">About me</h2>
     <div class="mt-3 space-y-4 will-change-contents" aria-labelledby="about-section">
       <p class="font-paragraph text-lg will-change-contents md:text-xl">
-        Hi, I'm a Frontend/Fullstack Developer with a focus on Web-Development, specifically UI/UX, functionality and
-        clean code.
+        Hi, I'm a Fullstack Developer with a focus on Frontend and Web-Development, specifically UI/UX, functionality
+        and clean code.
       </p>
       <p class="font-paragraph text-lg will-change-contents md:text-xl">
         With over 7 years of professional experience, I have a good understanding of diverse frontend topics such as
