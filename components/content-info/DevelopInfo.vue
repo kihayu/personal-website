c9880bbed9065187fe1dbf5ae383a786554d96d4
Keanu Hie <hie.keanu@gmail.com>
2025-02-21 02:04:27 +0100

    feat: add developinfo component with sample text and animation

---
+++ components/content-info/DevelopInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex h-full w-full flex-row gap-x-6">
+    <p
+      class="font-paragraph h-full w-full text-lg first-letter:float-left first-letter:mt-2 first-letter:pr-1 first-letter:text-6xl first-letter:leading-10"
+    >
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur aliquam magni exercitationem aut ducimus
+      doloribus, mollitia nostrum recusandae soluta. Necessitatibus magnam quae laborum dicta, minima ipsam voluptas
+      iure pariatur provident!
+    </p>
+    <div class="hidden h-full rounded-xl bg-stone-900 p-4 lg:block lg:min-w-1/2 xl:min-w-[45%]">
+      <CodeAnimation font-size="text-base" :greeting-code="false" loop show-pause-button />
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import CodeAnimation from '~/components/animations/CodeAnimation.vue'
+</script>
+
+<style scoped>
+.drop-cap::first-letter {
+  float: left;
+  font-size: 3em;
+  line-height: 0.8;
+  padding-right: 0.1em;
+  margin-top: 0.1em;
+}
+</style>
