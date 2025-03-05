eb3005927a45f0a29b6bfd284b93733337de5769
Keanu Hie <hie.keanu@gmail.com>
2025-03-05 01:30:53 +0100

    refactor: project card filled with props and add link

--- components/projects/ProjectCard.vue
+++ components/projects/ProjectCard.vue
@@ -1,15 +1,36 @@
 <template>
   <div
-    class="flex h-32 flex-col rounded-lg border border-neutral-400 p-4 hover:cursor-pointer hover:[&_.link-arrows]:underline"
+    class="relative flex min-h-fit flex-col rounded-lg border border-neutral-400 p-4 text-white no-underline hover:cursor-pointer hover:[&_.link-arrows]:underline"
   >
-    <h3 class="font-title text-2xl font-bold">Project Title</h3>
-    <span class="font-paragraph flex-1 text-lg">Project Description</span>
-    <div class="flex justify-between">
+    <h3 class="font-title text-2xl font-bold">{{ title }}</h3>
+    <span class="font-paragraph flex-1 text-lg">{{ description }}</span>
+    <div class="mt-6 flex justify-between">
       <div class="flex flex-row gap-2">
-        <span class="font-title rounded bg-neutral-700 px-2 py-1 text-sm text-white">Frontend</span>
-        <span class="font-title rounded bg-neutral-700 px-2 py-1 text-sm text-white">Backend</span>
+        <span
+          v-for="technology in technologies"
+          :key="technology"
+          class="font-title rounded bg-neutral-700 px-2 py-1 text-sm text-white"
+        >
+          {{ technology }}
+        </span>
       </div>
-      <span class="link-arrows select-none">>>></span>
+      <NuxtLink
+        :to="`/projects/${projectName}`"
+        class="link-arrows select-none after:absolute after:top-0 after:right-0 after:bottom-0 after:left-0 after:content-['\a']"
+      >
+        >>>
+      </NuxtLink>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+export interface ProjectCardProps {
+  title: string
+  description: string
+  technologies: string[]
+  projectName: number
+}
+
+defineProps<ProjectCardProps>()
+</script>
