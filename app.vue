fc90ac8722789413a22218f32a4630a5c1ebc86b
Keanu Hie <hie.keanu@gmail.com>
2025-03-05 19:48:07 +0100

    chore: add WebSite jsonld data

--- app.vue
+++ app.vue
@@ -2,4 +2,21 @@
   <NuxtPage />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  name: 'Keanu Hie · Portfolio',
+  url: 'https://www.keanuhie.com/',
+}
+
+useHead({
+  script: [
+    {
+      hid: 'breadcrumbs-json-ld',
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify(jsonLd),
+    },
+  ],
+})
+</script>
