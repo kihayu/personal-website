cca478f61e8a3a69946a493d7659daa41fd72827
Keanu Hie <hie.keanu@gmail.com>
2025-03-06 14:17:29 +0100

    fix: card not clickable on desktop

--- components/projects/ProjectCard.vue
+++ components/projects/ProjectCard.vue
@@ -1,9 +1,8 @@
 <template>
-  <div
+  <NuxtLink
+    :to="`/projects/${projectName}`"
     class="relative flex min-h-fit flex-col rounded-lg border border-neutral-400 p-4 text-white no-underline hover:cursor-pointer hover:[&_.link-arrows]:scale-120"
     tabindex="0"
-    @keydown.enter="navigate"
-    @keydown.space.prevent="navigate"
   >
     <h3 class="font-title text-2xl font-bold">{{ title }}</h3>
     <span class="font-paragraph flex-1 text-lg">{{ leadText }}</span>
@@ -17,15 +16,11 @@
           {{ technology }}
         </span>
       </div>
-      <NuxtLink
-        :to="`/projects/${projectName}`"
-        class="link-arrows self-end select-none after:absolute after:top-0 after:right-0 after:bottom-0 after:left-0 after:content-['\a']"
-        tabindex="-1"
-      >
+      <div class="link-arrows self-end select-none">
         <ChevronsRightIcon class="link-arrows transition duration-150" />
-      </NuxtLink>
+      </div>
     </div>
-  </div>
+  </NuxtLink>
 </template>
 
 <script setup lang="ts">
