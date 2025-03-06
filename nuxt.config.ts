d62bd7851ddb8fd3fab54c4e4ed57821d2870b4b
Keanu Hie <hie.keanu@gmail.com>
2025-03-07 00:13:50 +0100

    chore: improve meta title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
       bodyAttrs: {
         class: 'font-primary box-border bg-stone-900 text-white lg:px-16 xl:px-32',
       },
-      title: 'Keanu Hie - Fullstack Developer',
+      title: 'Keanu Hie · Developer Portfolio',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -47,14 +47,14 @@ export default defineNuxtConfig({
           content:
             'Fullstack Developer, Frontend Development, Backend Development, Vue.js, TypeScript, Node.js, Python, UI/UX Design, Figma, Web Accessibility, DevOps, Git, Linux, Vercel, Netlify, Bun, Game Development, LLM, AI, Vienna',
         },
-        { property: 'og:title', content: 'Keanu Hie - Fullstack Developer' },
+        { property: 'og:title', content: 'Keanu Hie · Developer Portfolio' },
         {
           property: 'og:description',
           content:
             'Fullstack developer crafting modern web experiences with Vue.js, TypeScript, and Python. Focused on accessible design, good looking user interface, and scalable solutions.',
         },
         { property: 'og:type', content: 'website' },
-        { name: 'twitter:title', content: 'Keanu Hie - Fullstack Developer' },
+        { name: 'twitter:title', content: 'Keanu Hie · Developer Portfolio' },
         {
           name: 'twitter:description',
           content:
