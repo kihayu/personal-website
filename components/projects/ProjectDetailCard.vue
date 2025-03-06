ac1d15365eb8a4fb110b40695aee12d37b2f914a
Keanu Hie <hie.keanu@gmail.com>
2025-03-06 22:17:16 +0100

    feat: activate project detail back button on mobile

--- components/projects/ProjectDetailCard.vue
+++ components/projects/ProjectDetailCard.vue
@@ -9,7 +9,7 @@
       </div>
       <button
         @click="closeDetails"
-        class="flex translate-y-1 cursor-pointer justify-center text-neutral-400 transition-all duration-300 lg:hover:scale-125 lg:active:scale-90"
+        class="flex translate-y-1 cursor-pointer justify-center text-neutral-400 transition-all duration-300 lg:hover:translate-y-2 lg:hover:scale-125 lg:active:scale-90"
         aria-label="Close project details"
       >
         <XIcon class="h-7 w-7 translate-y-0.25" :stroke-width="2.5" tabindex="0" />
@@ -34,6 +34,7 @@
 <script setup lang="ts">
 import { XIcon } from 'lucide-vue-next'
 import type { Project } from '~/types/project'
+import { isMobile } from '~/utils/isMobile'
 
 export interface ProjectDetailCardProps {
   project: Project
@@ -48,5 +49,28 @@ const emit = defineEmits<ProjectDetailCardEmits>()
 
 const closeDetails = (): void => {
   emit('close')
+}
+
+onMounted(() => {
+  if (!isMobile.value) {
+    return
+  }
+
+  window.history.pushState({ showProjectDetail: true }, '')
+  window.addEventListener('popstate', handlePopState)
+})
+
+onUnmounted(() => {
+  if (!isMobile.value) {
+    return
+  }
+
+  window.removeEventListener('popstate', handlePopState)
+})
+
+const handlePopState = (event: PopStateEvent) => {
+  if (event.state === null || !event.state.showProjectDetail) {
+    emit('close')
+  }
 }
 </script>
