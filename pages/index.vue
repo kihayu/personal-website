904d86c94a9064d5d338af7f5f8f12e611021fc1
Keanu Hie <hie.keanu@gmail.com>
2025-03-06 22:16:35 +0100

    fix: styling issue between sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <!-- Root container needed! -->
 <template>
-  <div id="root">
+  <div id="root" class="flex flex-col gap-y-4">
     <MainSection class="mx-4 max-w-[1280px] lg:min-h-screen" />
     <ProjectSection class="mx-4 min-h-screen max-w-[1280px]" />
   </div>
