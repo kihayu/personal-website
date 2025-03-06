65456ebb41012a89192b535326876d74e15581d4
Keanu Hie <hie.keanu@gmail.com>
2025-03-06 13:55:09 +0100

    fix: adjust width of project list

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex w-full flex-col items-center justify-center gap-y-4">
     <MainSection class="mx-4 max-w-[1280px] pt-4 lg:min-h-screen lg:pt-12 2xl:pt-16" />
-    <ProjectSection class="min-h-screen w-full px-4" />
+    <ProjectSection class="mx-4 min-h-screen max-w-[1280px]" />
   </div>
 </template>
 
