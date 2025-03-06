4702dd33b150c63f592175fc44095e59960fecec
Keanu Hie <hie.keanu@gmail.com>
2025-03-06 13:02:41 +0100

    refactor: improve accessibility for project cards

--- components/projects/ProjectCard.vue
+++ components/projects/ProjectCard.vue
@@ -1,6 +1,9 @@
 <template>
   <div
-    class="relative flex min-h-fit flex-col rounded-lg border border-neutral-400 p-4 text-white no-underline hover:cursor-pointer hover:[&_.link-arrows]:underline"
+    class="relative flex min-h-fit flex-col rounded-lg border border-neutral-400 p-4 text-white no-underline hover:cursor-pointer hover:[&_.link-arrows]:scale-120"
+    tabindex="0"
+    @keydown.enter="navigate"
+    @keydown.space.prevent="navigate"
   >
     <h3 class="font-title text-2xl font-bold">{{ title }}</h3>
     <span class="font-paragraph flex-1 text-lg">{{ description }}</span>
@@ -17,14 +20,17 @@
       <NuxtLink
         :to="`/projects/${projectName}`"
         class="link-arrows select-none after:absolute after:top-0 after:right-0 after:bottom-0 after:left-0 after:content-['\a']"
+        tabindex="-1"
       >
-        >>
+        <ChevronsRightIcon class="link-arrows transition duration-150" />
       </NuxtLink>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ChevronsRight as ChevronsRightIcon } from 'lucide-vue-next'
+
 export interface ProjectCardProps {
   title: string
   description: string
@@ -32,5 +38,10 @@ export interface ProjectCardProps {
   projectName: number
 }
 
-defineProps<ProjectCardProps>()
+const props = defineProps<ProjectCardProps>()
+
+const router = useRouter()
+const navigate = () => {
+  router.push(`/projects/${props.projectName}`)
+}
 </script>
