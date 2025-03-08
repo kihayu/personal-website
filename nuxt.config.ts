d2eb320003f907a29b8769a97208d2341c83d13a
Keanu Hie <hie.keanu@gmail.com>
2025-03-08 10:23:24 +0100

    chore: add more twitter meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -63,11 +63,17 @@ export default defineNuxtConfig({
           property: 'og:image:alt',
           content: 'A cover photo of Keanu Hie with the text "Turning Code into Compelling User Experiences"',
         },
+        { name: 'twitter:card', content: 'summary_large_image' },
         { name: 'twitter:title', content: 'Keanu Hie · Developer Portfolio' },
         {
           name: 'twitter:description',
           content:
             'Fullstack developer crafting modern web experiences with Vue.js, TypeScript, and Python. Focused on accessible design, good looking user interface, and scalable solutions.',
+        },
+        { name: 'twitter:image', content: 'https://www.keanuhie.com/images/og-image.png' },
+        {
+          name: 'twitter:image:alt',
+          content: 'A cover photo of Keanu Hie with the text "Turning Code into Compelling User Experiences"',
         },
       ],
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
