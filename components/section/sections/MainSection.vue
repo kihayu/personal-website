956c9a22c1ff0a3f108fe67aa7df4978de150681
Keanu Hie <hie.keanu@gmail.com>
2025-05-15 10:38:44 +0200

    refactor: remove max-height constraint from section

--- components/section/sections/MainSection.vue
+++ components/section/sections/MainSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <SectionComponent class="gap-y-4 lg:gap-y-8" :class="{ 'max-h-screen': selectedSection.trim() !== '' }">
+  <SectionComponent class="gap-y-4 lg:gap-y-8">
     <div
       class="flex w-full flex-col justify-center gap-x-4 gap-y-4 sm:grid sm:grid-cols-[250px_1fr] md:grid-rows-1 lg:grid-cols-[300px_1fr] lg:gap-x-8 lg:gap-y-8"
     >
