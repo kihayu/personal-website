90052a50917d05f46c527c829dd64512b66072d2
Keanu Hie <hie.keanu@gmail.com>
2026-09-09 19:17:34 +0200

    style(layout): pad the page wrapper below the extra-large breakpoint

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-4 flex w-full flex-col items-center justify-center lg:mt-12 2xl:mt-16">
+  <div class="mt-4 flex w-full flex-col items-center justify-center px-4 lg:mt-12 xl:p-0 2xl:mt-16">
     <slot />
   </div>
 </template>
